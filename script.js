384dabd434cfdbf57bfcaf91d0cdad1eb878eca5
grace moon <hi.gracemoon@gmail.com>
2018-01-03 19:19:59 -0500

    jquery slider

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.gallery').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  centerMode: true,
+	  variableWidth: true,
+	 //    dots: true,
+  // infinite: true,
+  // speed: 300,
+  // slidesToShow: 1,
+  // adaptiveHeight: true
+  });
+});
+
+
+// $(".single-item").slick({
+// 	dots: true
+// });
+
+
+
+        // $(document).ready(function(){
+        //     $('.gallery').slick();
+        // });
